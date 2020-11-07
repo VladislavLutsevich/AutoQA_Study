dd536b32b181b6bac8bfdca3d3bf6eb9b41784e7
vladl <vladlutsevich21@gmail.com>
2020-11-08 02:13:01 +0300

    Thread.sleep replacement.

--- src/main/java/by/stormnet/yandex/mailProfileSection/MailSendingForm.java
+++ src/main/java/by/stormnet/yandex/mailProfileSection/MailSendingForm.java
@@ -4,7 +4,6 @@ import by.stormnet.utils.WaitManager;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -18,7 +17,7 @@ public class MailSendingForm {
     private static final Logger logger = LogManager.getLogger(MailSendingForm.class);
     private static final By MAIL_TO_FIELD_LOCATOR = (By.cssSelector("div.MultipleAddressesDesktop.ComposeRecipients-MultipleAddressField.tst-field-to > div > div > div > div"));
     private static final By FILE_ATTACH_LOCATOR = (By.cssSelector(".ComposeAttachmentSourcesMenu-FileInput"));
-    private static final By DONE_FILE_ATTACH_LOCATOR = (By.cssSelector(".ComposeAttachments-Item a[href]"));
+    private static final By FILE_ATTACHING_PROGRESS_BAR_LOCATOR = (By.cssSelector("div.ComposeAttachmentsLoadingProgress"));
     private static final By MAIL_SEND_BUTTON = (By.cssSelector("div.ComposeSendButton_desktop > button"));
     private static final By SUCCESS_MAIL_SENDING = (By.cssSelector(".ComposeDoneScreen-Title"));
 
@@ -45,7 +44,7 @@ public class MailSendingForm {
 
     public void attachFile(File file) throws InterruptedException {
         getFileAttachInput().sendKeys(file.getAbsolutePath());
-        Thread.sleep(Integer.parseInt(getProperty("sleep_time")));
+        new WaitManager(webDriver).waitInvisibleStatePageElement(FILE_ATTACHING_PROGRESS_BAR_LOCATOR);
     }
 
     public void sendMail() {
